4166ef568d69f22269fa67f2a019b68ef11f0e4b
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-01-15 11:51:44 +0100

    job06

---
+++ jour03/job06/index.php
@@ -0,0 +1,13 @@
+<?php
+// Définition de la chaîne
+$str = "Les choses que l'on Possède finissent par nous posséder.";
+
+// Affichage de la chaîne à l'envers
+echo strrev($str);
+?>
+.reddésop sun arp tnessif édessop n'oeluq sehsoc seL
+
+
+
+
+
