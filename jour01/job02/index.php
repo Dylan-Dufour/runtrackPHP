c69786151f229d5d0e788094fcba9210ffe408a7
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-01-15 10:16:48 +0100

    job02

--- jour01/job02/index.php
+++ jour01/job02/index.php
@@ -1,39 +1,12 @@
 <?php
-  // Création de la variable str
-  $str = "LaPlateforme";
-  $str2 = "vive";
-  $str3 = "Vive LaPlateforme!"
+// Affichage du message "Hello LaPlateforme!" une première fois
+echo "Hello LaPlateforme!"; // Ceci affiche le message
 
-  
-  // Affichage du contenu de la variable str
-echo $str;
+/*
+Affichage du message "Hello LaPlateforme!" une deuxième fois
+dans un bloc de commentaire multi-lignes
+*/
+echo "Hello LaPlateforme!"; // Affichage du message une deuxième fois
 ?>
-<?php
-  // Création de la variable val et affectation de la valeur 6
-  $val = 6;
-  
-  // Affichage du contenu de la variable val
-  echo $val . "<br>";
-  
-  // Ajout de 4 à la variable val
-  $val += 4;
-  
-  // Affichage du nouveau contenu de la variable val
-  echo $val . "<br>";
-?>
-<?php
-  // Création de la variable myBool et affectation de la valeur true
-  $myBool = true;
-  
-  // Affichage du contenu de la variable myBool (true)
-  echo $myBool ? 'true' : 'false';  // Affiche "true"
-  echo "<br>";
-  
-  // Affectation de false à la variable myBool
-  $myBool = false;
-  
-  // Affichage du contenu de la variable myBool (false)
-  echo $myBool ? 'true' : 'false';  // Affiche "false"
-  echo "<br>";
-?>
+
 
