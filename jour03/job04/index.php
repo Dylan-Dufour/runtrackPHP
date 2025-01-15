f95ce66205def6ba4048b2998f77beedf792bb4d
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-01-15 11:40:36 +0100

    job04

---
+++ jour03/job04/index.php
@@ -0,0 +1,13 @@
+<?php
+// Définition de la chaîne
+$str = "Dans l'espace, personne ne vous entend crier";
+
+// Comptage des caractères dans la chaîne
+$nombre_de_caracteres = strlen($str);
+
+// Affichage du résultat
+echo "Le nombre de caractères dans la chaîne est : " . $nombre_de_caracteres;
+?>
+
+
+
