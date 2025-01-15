5925736fe31112c3fd18146576e5c7a110c7e051
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-01-15 10:05:04 +0100

    job2

---
+++ jour01/job02/index.php
@@ -0,0 +1,39 @@
+<?php
+  // Création de la variable str
+  $str = "LaPlateforme";
+  $str2 = "vive";
+  $str3 = "Vive LaPlateforme!"
+
+  
+  // Affichage du contenu de la variable str
+echo $str;
+?>
+<?php
+  // Création de la variable val et affectation de la valeur 6
+  $val = 6;
+  
+  // Affichage du contenu de la variable val
+  echo $val . "<br>";
+  
+  // Ajout de 4 à la variable val
+  $val += 4;
+  
+  // Affichage du nouveau contenu de la variable val
+  echo $val . "<br>";
+?>
+<?php
+  // Création de la variable myBool et affectation de la valeur true
+  $myBool = true;
+  
+  // Affichage du contenu de la variable myBool (true)
+  echo $myBool ? 'true' : 'false';  // Affiche "true"
+  echo "<br>";
+  
+  // Affectation de false à la variable myBool
+  $myBool = false;
+  
+  // Affichage du contenu de la variable myBool (false)
+  echo $myBool ? 'true' : 'false';  // Affiche "false"
+  echo "<br>";
+?>
+
